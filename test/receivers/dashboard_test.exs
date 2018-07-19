6ca87d0d63b19506c5741c858c269443fdeef7a5
Felipe Ripoll <ferigis@gmail.com>
2018-07-18 19:12:29 -0600

    [#30] sending last metrics when the dashboard connects to the backend

--- test/receivers/dashboard_test.exs
+++ test/receivers/dashboard_test.exs
@@ -8,8 +8,9 @@ defmodule Receivers.DashboardTest do
   defmodule Client do
     use WebSockex
 
-    def send(client, message) do
+    def send(client, message, caller) do
       WebSockex.send_frame(client, {:text, message})
+      Kernel.send(caller, :message_sent)
     end
 
     def start_link(address, state, opts \\ []) do
@@ -45,10 +46,32 @@ defmodule Receivers.DashboardTest do
     assert_receive ^expected_message, 20_000
   end
 
+  test "When the Dashboard connects already exists last metrics in the Backend" do
+
+    # we send the metrics twice in order to complete all the cases in the Receiver
+    # when the ets doesn't have the agentid and when it already exists
+    POABackend.Metric.add(:ethereum_metrics, [information_message(), stats_message(), information_message()])
+    POABackend.Metric.add(:ethereum_metrics, [information_message2(), stats_message2()])
+
+    Client.start_link("http://localhost:8181/ws", self(), [{:extra_headers, [{"wssecret", "mywssecret"}]}])
+
+    expected_information_message = expected_information_message()
+    expected_stats_message = expected_stats_message()
+    expected_information_message2 = expected_information_message2()
+    expected_stats_message2 = expected_stats_message2()
+
+    assert_receive ^expected_information_message, 20_000
+    assert_receive ^expected_stats_message, 20_000
+    assert_receive ^expected_information_message2, 20_000
+    assert_receive ^expected_stats_message2, 20_000
+  end
+
   test "handle messages from the client to the server (test coverage)" do
     {:ok, client} = Client.start_link("http://localhost:8181/ws", self(), [{:extra_headers, [{"wssecret", "mywssecret"}]}])
 
-    Client.send(client, "hello")
+    Client.send(client, "hello", self())
+
+    assert_receive :message_sent, 20_000
   end
 
   test "http call to the server (test coverage)" do
@@ -65,4 +88,35 @@ defmodule Receivers.DashboardTest do
     Message.new("agentid1", :ethereum_metric, :data, %{a: "a", b: "b", c: "c"})
   end
 
+  defp information_message do
+    Message.new("agentid", :ethereum_metric, :data, %{"type" => "information"})
+  end
+
+  defp stats_message do
+    Message.new("agentid", :ethereum_metric, :data, %{"type" => "statistics"})
+  end
+
+  defp information_message2 do
+    Message.new("agentid2", :ethereum_metric, :data, %{"type" => "information"})
+  end
+
+  defp stats_message2 do
+    Message.new("agentid2", :ethereum_metric, :data, %{"type" => "statistics"})
+  end
+
+  defp expected_information_message do
+    "{\"data\":{\"type\":\"information\"},\"agent_id\":\"agentid\"}"
+  end
+
+  defp expected_stats_message do
+    "{\"data\":{\"type\":\"statistics\"},\"agent_id\":\"agentid\"}"
+  end
+
+  defp expected_information_message2 do
+    "{\"data\":{\"type\":\"information\"},\"agent_id\":\"agentid2\"}"
+  end
+
+  defp expected_stats_message2 do
+    "{\"data\":{\"type\":\"statistics\"},\"agent_id\":\"agentid2\"}"
+  end
 end
